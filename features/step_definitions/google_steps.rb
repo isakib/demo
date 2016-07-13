b12ef3e0c66bfbc58160995ee4e3a24b8fa65767
Sakib <isakib@gmail.com>
2016-07-13 16:47:38 +0600

    cool

---
+++ features/step_definitions/google_steps.rb
@@ -0,0 +1,18 @@
+Given(/^I am on the home page$/) do
+puts  visit "http://www.google.com/ncr"
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |element, text|
+ fill_in element, with: text
+ #page.driver.debug
+end
+
+Then(/^I should see "(.*?)"$/) do |text|
+  page.should have_content text
+  # page.driver.resize(20,30)
+  page.save_screenshot("test.png")
+  # puts page.within_window
+  # puts page.driver.network_traffic
+  #puts page.driver.cookies
+  #page.response_headers.to_a
+end
